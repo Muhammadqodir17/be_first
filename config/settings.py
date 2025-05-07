f7f7c39631ec833e584b6965012893ffe196a8de
Muhammadqodir <km1728.uz@gmail.com>
2025-05-07 21:03:52 +0500

    middlewares.py bug fixed

--- config/settings.py
+++ config/settings.py
@@ -198,8 +198,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=17),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=17),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
