43b19d93b721a1c824c76066fcdb14080e704085
Muhammadqodir <km1728.uz@gmail.com>
2025-05-14 22:24:39 +0500

    bugs fixed

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import User, SMSCode, TemporaryPassword
+from .models import (
+    User,
+    SMSCode,
+    TemporaryPassword,
+    OTPRegisterResend,
+    OTPSetPassword
+)
 
 admin.site.register(User)
 admin.site.register(SMSCode)
 admin.site.register(TemporaryPassword)
+admin.site.register(OTPRegisterResend)
+admin.site.register(OTPSetPassword)
