3fb841178b7652a1c8b120d476bc4a1e08079f47
Muhammadqodir <km1728.uz@gmail.com>
2025-06-07 18:34:43 +0500

    dynamic info added

--- payment/tests.py
+++ payment/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+# expiry = '2509'
+# year = expiry[:2]
+# month = expiry[2:]
+# res = year + month
+# print(res)
 
-# Create your tests here.
+# expiry = '09/25'
+# month = expiry[:2]
+# year = expiry[3:]
+# res = year + month
+# print(res)
