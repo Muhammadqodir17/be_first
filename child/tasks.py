27f903435f7c339918ca5af724c1a1188574e35d
Muhammadqodir <km1728.uz@gmail.com>
2025-06-11 19:16:06 +0500

    dynamic info added

--- child/tasks.py
+++ child/tasks.py
@@ -1,23 +1,31 @@
 import os
+
 from celery import shared_task
 from django.core.files import File
 from django.core.files.storage import default_storage
+
 from konkurs.models import ChildWork, Participant
+from rest_framework.response import Response
+from rest_framework import status
+
 
 @shared_task(name='celery_tasks.tasks.bulk_works_create')
 def bulk_works_create(participant_id, file_paths):
-    participant = Participant.objects.get(id=participant_id)
-    child_works = []
+    participant = Participant.objects.filter(id=participant_id).first()
+    if participant is None:
+        return 'Participant not found'
 
+    child_works = []
     for path in file_paths:
         with default_storage.open(path, 'rb') as f:
-            file_content = File(f, name=os.path.basename(path))
+            file_obj = File(f, name=os.path.basename(path))
             child_works.append(
                 ChildWork(
                     participant=participant,
                     competition=participant.competition,
-                    files=file_content
+                    files=file_obj
                 )
             )
 
     ChildWork.objects.bulk_create(child_works)
+    return 'Success'
